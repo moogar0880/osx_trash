0fa44e43ae5d03dcf282cb50a1b119f9b24a225a
moogar0880 <moogar0880@gmail.com>
2015-09-24 00:39:26 -0400

    Fix installation

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from setuptools import setup
 from osx_trash import __version__
 
 
-packages = ['osx_trash']
-
 setup(name='osx_trash',
       version=__version__,
       description=('An extendible utility for moving files to the trash, '
@@ -15,10 +13,9 @@ setup(name='osx_trash',
       maintainer='Jonathan Nappi',
       maintainer_email='moogar@comcast.net',
       license='http://www.gnu.org/copyleft/gpl.html',
-      package_dir={'osx_trash': 'osx_trash'},
       platforms=['OS X'],
       url='https://github.com/moogar0880/osx_trash',
-      packages=packages,
+      py_modules=['osx_trash'],
       zip_safe=False,
       entry_points={
           'console_scripts': ['trash = osx_trash:main']
